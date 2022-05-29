b1a6d120bc73a8f9f2a0bf5e8efaa81ca9d5155c
John-Paul Teti <jp@jpteti.com>
2022-05-28 23:53:03 -0400

    wip - functionality works

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.min.css';
+import Checker from './Checker';
+import { Section, Container, Heading } from 'react-bulma-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Section>
+      <Container>
+        <Heading>Wordle Solution Checker</Heading>
+        <Checker />
+      </Container>
+    </Section>
   );
 }
 
